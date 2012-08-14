f9245ce9daf1d54976aae061552ba898164b304e
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-08-14 18:11:26 +0100

    Add JSON file output to version info

--- build.sbt
+++ build.sbt
@@ -6,4 +6,8 @@ version := "2.3-SNAPSHOT"
 
 sbtPlugin := true
 
+resolvers += "repo.codahale.com" at "http://repo.codahale.com"
+
+libraryDependencies += "com.codahale" %% "jerkson" % "0.5.0"
+
 seq(scalariformSettings: _*)
--- src/main/scala/com/gu/versioninfo/VersionInfo.scala
+++ src/main/scala/com/gu/versioninfo/VersionInfo.scala
@@ -5,18 +5,25 @@ import java.util.Date
 import sbt._
 import Keys._
 import java.lang.System
+import com.codahale.jerkson.Json._
 
 object VersionInfo extends Plugin {
 
   val branch = SettingKey[String]("version-branch")
   val buildNumber = SettingKey[String]("version-build-number")
   val vcsNumber = SettingKey[String]("version-vcs-number")
+  val vcsRootUrl = SettingKey[String]("version-vcsroot-url")
+  val vcsRootBranch = SettingKey[String]("version-vcsroot-branch")
 
   override val settings = Seq(
     buildNumber := System.getProperty("build.number", "DEV"),
     branch := System.getProperty("build.vcs.branch", "DEV"),
     vcsNumber := System.getProperty("build.vcs.number", "DEV"),
-    resourceGenerators in Compile <+= (resourceManaged in Compile, branch, buildNumber, vcsNumber, streams) map buildFile
+    vcsRootUrl := System.getProperty("vcsroot.url", "DEV"),
+    vcsRootBranch := System.getProperty("vcsroot.branch", "DEV"),
+
+    resourceGenerators in Compile <+= (resourceManaged in Compile, branch, buildNumber, vcsNumber, streams) map buildFile,
+    resourceGenerators in Compile <+= (resourceManaged in Compile, buildNumber, vcsNumber, vcsRootUrl, vcsRootBranch, streams, name) map buildJsonFile
   )
 
   def buildFile(outDir: File, branch: String, buildNum: String, vcsNum: String, s: TaskStreams) = {
@@ -36,5 +43,36 @@ object VersionInfo extends Plugin {
     IO.write(versionFile, versionFileContents mkString "\n")
 
     Seq(versionFile)
+  }
+
+  def buildJsonFile(outDir: File, buildNum: String, vcsNum: String, vcsUrl: String, vcsBranch: String, s: TaskStreams, projectName: String) = {
+    case class Git(url: String, branch: String, commit: String) {
+      val `type`: String = "git"
+    }
+
+    case class Environment(
+      `built-by`: Option[String],
+      `built-on`: Option[String],
+      `built-jdk`: Option[String])
+
+    case class Version(
+      label: String,
+      release: String,
+      build: String,
+      date: String,
+      `version-control`: Git,
+      description: Option[String],
+      environment: Option[Environment])
+
+    val environment = Environment(None, None, None)
+    val git = Git(vcsUrl, vcsBranch, vcsNum)
+    val version = Version("", "", buildNum, new Date().toString, git, None, None)
+    val jsonContent = generate(version)
+
+    val versionJsonFile = outDir / ("%s.version.json" format projectName)
+    s.log.debug("Writing to %s:\n   %s" format (versionJsonFile, jsonContent))
+
+    IO.write(versionJsonFile, jsonContent)
+    Seq(versionJsonFile)
   }
 }
