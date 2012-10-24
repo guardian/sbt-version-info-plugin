f534d9c8f567754a44bddcc637cec6277dc421b6
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-10-24 17:12:45 +0100

    Dequote and trim version.txt string entries.

--- src/main/scala/com/gu/versioninfo/VersionInfo.scala
+++ src/main/scala/com/gu/versioninfo/VersionInfo.scala
@@ -8,6 +8,10 @@ import java.lang.System
 
 object VersionInfo extends Plugin {
 
+  implicit def string2Dequote(s: String) = new {
+    lazy val dequote = s.replace("\"", "")
+  }
+
   val branch = SettingKey[String]("version-branch")
   val buildNumber = SettingKey[String]("version-build-number")
   val vcsNumber = SettingKey[String]("version-vcs-number")
@@ -21,9 +25,9 @@ object VersionInfo extends Plugin {
 
   def buildFile(outDir: File, branch: String, buildNum: String, vcsNum: String, s: TaskStreams) = {
     val versionInfo = Map(
-      "Revision" -> vcsNum,
-      "Branch" -> branch,
-      "Build" -> buildNum,
+      "Revision" -> vcsNum.dequote.trim,
+      "Branch" -> branch.dequote.trim,
+      "Build" -> buildNum.dequote.trim,
       "Date" -> new Date().toString,
       "Built-By" -> System.getProperty("user.name", "<unknown>"),
       "Built-On" -> InetAddress.getLocalHost.getHostName)
