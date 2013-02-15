ce7946cd3b0edf481f70eb667b5bc7cecbe59485
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-02-15 18:34:34 +0000

    WIP for setting out new version-info plugin

--- src/main/scala/com/gu/versioninfo/VersionInfo.scala
+++ src/main/scala/com/gu/versioninfo/VersionInfo.scala
@@ -7,6 +7,7 @@ import Keys._
 import java.lang.System
 import scala.collection.JavaConversions._
 import java.io.FileInputStream
+import util.parsing.json.JSONObject
 
 object VersionInfo extends Plugin {
 
@@ -14,22 +15,17 @@ object VersionInfo extends Plugin {
     lazy val dequote = s.replace("\"", "")
   }
 
-  val buildNumber = SettingKey[String]("version-build-number")
-  val vcsBranch = SettingKey[String]("version-branch")
-  val vcsNumber = SettingKey[String]("version-vcs-number")
-  val vcsUrl = SettingKey[String]("version-vcs-url")
+  def getSystemProperty(key: String): Option[String] = {
+    try {
+      Some(System.getProperty(key))
+    } catch {
+      case e:NullPointerException => None
+      case e:IllegalArgumentException => None
+    }
+  }
 
-  override val settings = Seq(
-    buildNumber := System.getProperty("build.number", "DEV"),
-    vcsBranch := System.getProperty("teamcity.build.branch", "DEV"),
-    vcsNumber := System.getProperty("build.vcs.number", "DEV"),
-    vcsUrl := System.getProperty("vcsroot.url", ""),
-
-    resourceGenerators in Compile <+= (resourceManaged in Compile, name, vcsUrl, vcsBranch, vcsNumber, buildNumber, streams) map buildFile
-  )
-
-  def buildFile(outDir: File, name: String, vcsUrl: String, branch: String, vcsNum: String, buildNum: String, s: TaskStreams) = {
-    val tcProps = try {
+  def readTeamCityProperties(buildProperties: File): Map[String, String] = {
+    try {
       val tcPropFile = System.getProperty("build.properties.file")
       val tcProperties = new Properties()
       tcProperties.load(new FileInputStream(new File(tcPropFile)))
@@ -41,6 +37,48 @@ object VersionInfo extends Plugin {
         s.log.warn("TeamCity properties file not found - not in TeamCity?")
         Map.empty
     }
+  }
+
+  val versionInfoDev = SettingKey[Boolean]("version-info-dev")
+  val teamcityBuildProperties = SettingKey[Option[File]]("teamcity-build-properties")
+
+  val projectDescription = SettingKey[Option[String]]("project-description")
+  val ciBuildNumber = SettingKey[Option[String]]("ci-build-number")
+  val vcsRevision = SettingKey[Option[String]]("vcs-revision")
+  val vcsBranch = SettingKey[Option[String]]("vcs-revision")
+
+  override val settings = {
+    Seq(
+      teamcityBuildProperties := getSystemProperty("build.properties.file").map(new File),
+      ciBuildNumber := getSystemProperty("build.number"),
+      vcsBranch := getSystemProperty("build.vcs.branch"),
+      vcsRevision := getSystemProperty("build.vcs.number"),
+      projectDescription := None,
+
+      versionInfoDev := ( getSystemProperty("build.properties.file").isEmpty && getSystemProperty("build.number").isEmpty
+      && getSystemProperty("build.branch").isEmpty && getSystemProperty("build.vcs.number").isEmpty ),
+
+      resourceGenerators in Compile <+= (
+        resourceManaged in Compile,
+        streams,
+        name,
+        versionInfoDev,
+        teamcityBuildProperties,
+        ciBuildNumber,
+        vcsRevision,
+        vcsBranch,
+        projectDescription
+      ) map buildFile
+    )
+  }
+
+  def buildFile(outDir: File, s: TaskStreams, name: String, versionInfoDev: Boolean,
+                teamcityBuildProperties: Option[File], ciBuildNumber: Option[String], vcsRevision: Option[String],
+                vcsBranch: Option[String], projectDescription: Option[String]) = {
+
+    val tcProps = teamcityBuildProperties.map(readTeamCityProperties).getOrElse(Map.empty)
+
+
 
     val versionInfo = tcProps ++ Map(
       "Revision" -> vcsNum.dequote.trim,
@@ -58,5 +96,40 @@ object VersionInfo extends Plugin {
     IO.write(versionFile, versionFileContents mkString "\n")
 
     Seq(versionFile)
+  }
+
+  def buildJson(outDir: File, buildNum: String, vcsNum: String, vcsUrl: String, vcsBranch: String, s: TaskStreams, projectName: String, versionName: String): String = {
+
+    val ciMap = Map(
+      "built-by" -> None,
+      "built-on" -> None,
+      "build-jdk" -> None,
+      "build-id" -> None,
+      "build-number" -> None,
+      "name" -> None,
+      "project" -> None,
+      "configuration" -> None
+    )
+    val vcsMap = Map(
+      "type" -> "git",
+      "revision" -> vcsNum,
+      "branch" -> vcsBranch,
+      "url" -> vcsUrl
+    )
+    val projectMap = Map(
+      "label" -> "",
+      "description" -> versionName,
+      "ci" -> ciMap,
+      "vcs" -> vcsMap
+    )
+
+    val json = JSONObject(Map(projectName -> projectMap))
+    val jsonContent = json.toString()
+
+    val versionJsonFile = outDir / ("%s.version.json" format projectName)
+    s.log.debug("Writing to %s:\n   %s" format (versionJsonFile, jsonContent))
+
+    IO.write(versionJsonFile, jsonContent)
+    Seq(versionJsonFile)
   }
 }
